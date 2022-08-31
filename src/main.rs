97e7026bb251b935c35ebdc07a3bc9d02fb1b6b6
Lukas Sestic <lukas.sestic@photomath.com>
2022-08-31 13:39:48 +0200

    feature: support key search

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,36 @@
+use serde_json::{self, json, Value};
 use std::io;
-use serde_json::{Value, self, json};
+
+fn pretty_print_value(value: &Value) {
+    pretty_print_value_w_prefix(value, "".to_owned());
+}
+
+fn pretty_print_value_w_prefix(value: &Value, prefix: String) {
+    println!(
+        "{} {}",
+        prefix,
+        serde_json::to_string_pretty(value).unwrap()
+    );
+}
+
+fn log_key(key: &String, source: &Value) {
+    match source.as_object() {
+        None => return,
+        Some(object) => object.iter().for_each(|i| {
+            let value = i.1;
+
+            if i.0 == key {
+                let key = i.0;
+
+                pretty_print_value_w_prefix(value, format!("{key} =>"));
+            }
+
+            log_key(key, value);
+        }),
+    }
+}
 
 fn main() -> io::Result<()> {
-
     let mut json_buffer = String::new();
 
     io::stdin().read_line(&mut json_buffer)?;
@@ -11,7 +39,13 @@ fn main() -> io::Result<()> {
 
     let obj: Value = serde_json::from_str(&json_buffer.to_owned())?;
 
-    println!("{}", serde_json::to_string_pretty(&obj).unwrap());
+    if let Some(searchedKey) = std::env::args().nth(1) {
+        log_key(&searchedKey, &obj);
+
+        return Ok(());
+    }
+
+    pretty_print_value(&obj);
 
     Ok(())
 }
